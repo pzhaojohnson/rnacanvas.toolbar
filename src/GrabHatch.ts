d7586a6eb7b23ed32a8d6593545589af2552f688
Philip Johnson <p.zhao.johnson@gmail.com>
2024-09-26 10:46:52 -0400

    Add tooltip to toolbar grab hatch

--- src/GrabHatch.ts
+++ src/GrabHatch.ts
@@ -1,12 +1,6 @@
 import * as styles from './GrabHatch.css';
 
-function Line() {
-  let line = document.createElement('div');
-
-  line.classList.add(styles.line);
-
-  return line;
-}
+import { detectMac } from '@rnacanvas/utilities';
 
 export function GrabHatch() {
   let grabHatch = document.createElement('div');
@@ -24,5 +18,35 @@ export function GrabHatch() {
     Line(),
   );
 
+  grabHatch.append(Tooltip());
+
   return grabHatch;
+}
+
+function Line() {
+  let line = document.createElement('div');
+
+  line.classList.add(styles.line);
+
+  return line;
+}
+
+let contextClick = detectMac() ? 'Ctrl-Click' : 'Right-click'
+
+function Tooltip() {
+  let tooltip = document.createElement('div');
+
+  tooltip.classList.add(styles['tooltip']);
+
+  let firstLine = document.createElement('p');
+  firstLine.classList.add(styles['tooltip-text']);
+  firstLine.textContent = 'Anywhere along the toolbar can be dragged.';
+  tooltip.append(firstLine);
+
+  let secondLine = document.createElement('p');
+  secondLine.classList.add(styles['tooltip-text']);
+  secondLine.textContent = `${contextClick} while dragging to avoid pressing buttons.`;
+  tooltip.append(secondLine);
+
+  return tooltip;
 }
