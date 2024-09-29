cbc4492e49fbd8e9bc199b32412e240fe154fa1b
Philip Johnson <p.zhao.johnson@gmail.com>
2024-09-29 09:52:02 -0400

    Remove appendTo() and remove() methods from toolbar toggle

--- src/ToolbarToggle.ts
+++ src/ToolbarToggle.ts
@@ -49,24 +49,6 @@ export class ToolbarToggle {
     this.domNode.addEventListener('click', () => this.press());
   }
 
-  /**
-   * Appends the DOM node corresponding to the toolbar toggle
-   * to the provided container node.
-   */
-  appendTo(container: Node): void {
-    container.appendChild(this.domNode);
-  }
-
-  /**
-   * Removes the DOM node corresponding to the toolbar toggle
-   * from any parent container node that it is in.
-   *
-   * Has no effect if the toolbar toggle had no parent container node to begin with.
-   */
-  remove(): void {
-    this.domNode.remove();
-  }
-
   press(): void {
     this.#targetApp.toolbar.reposition();
   }
