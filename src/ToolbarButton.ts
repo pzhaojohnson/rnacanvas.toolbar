bfc4bc1ba349f812d0fd5c8f7614a0c92e617310
Philip Johnson <p.zhao.johnson@gmail.com>
2024-10-06 21:04:09 -0400

    Prevent dragging of the toolbar when toolbar buttons are clicked normally

--- src/ToolbarButton.ts
+++ src/ToolbarButton.ts
@@ -1,11 +1,22 @@
 import * as styles from './ToolbarButton.css';
 
+import { detectMac } from '@rnacanvas/utilities';
+
 export function ToolbarButton(content: string | SVGSVGElement) {
   let toolbarButton = document.createElement('button');
 
   toolbarButton.classList.add(styles.toolbarButton);
 
   toolbarButton.append(content);
+
+  // prevent dragging of the toolbar when toolbar buttons are clicked normally
+  toolbarButton.addEventListener('mousedown', event => {
+    if (detectMac()) {
+      event.button == 0 && !event.ctrlKey ? event.stopPropagation() : {};
+    } else {
+      event.button == 0 ? event.stopPropagation() : {};
+    }
+  });
 
   return toolbarButton;
 }
