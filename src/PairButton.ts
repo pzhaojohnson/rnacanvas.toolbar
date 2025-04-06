0211e715e0b399f71661750b50578a2aded1f8b9
Philip Johnson <p.zhao.johnson@gmail.com>
2025-04-06 19:42:51 -0400

    Adjust styling

--- src/PairButton.ts
+++ src/PairButton.ts
@@ -29,7 +29,7 @@ export class PairButton<B extends Nucleobase, F> {
     this.domNode.classList.add(styles['pair-button']);
 
     let icon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    icon.setAttribute('viewBox', '0 0 22 22');
+    icon.setAttribute('viewBox', '-4 0 22 22');
     icon.setAttribute('width', '22');
     icon.setAttribute('height', '22');
 
--- src/UnpairButton.ts
+++ src/UnpairButton.ts
@@ -27,7 +27,7 @@ export class UnpairButton<B extends Nucleobase, F> {
     this.domNode.classList.add(styles['unpair-button']);
 
     let icon = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-    icon.setAttribute('viewBox', '0 0 22 22');
+    icon.setAttribute('viewBox', '-7 0 22 22');
     icon.setAttribute('width', '22');
     icon.setAttribute('height', '22');
 
